6d4588e253bfb0cde9dcb4be8204a4caa34955a5
javigar <javier.garcia.gonzalez1@everis.com>
2019-10-08 23:59:16 +0200

    arreglo home y nav bar

---
+++ src/app/shared/models/nav-bar.model.ts
@@ -0,0 +1,15 @@
+export interface NavData {
+    link: string;
+    class: string;
+    activateClass: string;
+    icon?: string;
+    text?: string;
+}
+
+export interface IconsFa {
+    faHome: any;
+    faHTML: any;
+    faCSS: any;
+    faAngular: any;
+    faJs: any;
+}
