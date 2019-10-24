98732091d2d7c9978a0717f3e6b716e9eb119802
Javier García <javilasalle@gmail.com>
2019-10-24 16:00:06 +0200

    added PWA

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Documentacion Blanca Javi
 Página con documentación sobre tecnologías FRONT
 
+![Front Logo](./src/assets/images/logo-font.png?raw=true "Front Logo")
+
 ## Authors
 
 * **Blanca Ros San Juan** - [BlancaRos](https://github.com/BlancaRos)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -13,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FontAwesomeModule,
     MatProgressSpinnerModule,
     SharedModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [
     AppComponent
