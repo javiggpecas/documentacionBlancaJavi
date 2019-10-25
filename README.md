fcd42c96d4f9d6a026c111d60d803a96d9015a34
Javier García <javilasalle@gmail.com>
2019-10-25 11:24:21 +0200

    icons for pwa

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Página con documentación sobre tecnologías FRONT
 
 * **Blanca Ros San Juan** - [BlancaRos](https://github.com/BlancaRos)
 * **Javier García González** - [javiggpecas](https://github.com/javiggpecas)
+
+## Internet Explorer
+
+Para poder iniciar la aplicacion en Internet Explorer es necesario ejecutar la aplicación en ECMAScript 5 (ES5) mediante el comando:
+    
+    ng serve --configuration es5
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,35 +5,35 @@ const routes: Routes = [
   {
     path: '',
     // por defecto, va a formularios
-    redirectTo: '/home',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de forms
+    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de home
     path: 'home',
     loadChildren: () => import('./containers/home/home.module').then(m => m.HomeModule)
   },
   {
-    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de home
+    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de angular
     path: 'angular',
     loadChildren: () => import('./containers/angular/angular.module').then(m => m.AngularModule)
   },
   {
-    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de angular
+    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de html
     path: 'html',
     loadChildren: () => import('./containers/html/html.module').then(m => m.HtmlModule)
   },
   {
-    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de html
+    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de css
     path: 'css',
     loadChildren: () => import('./containers/css/css.module').then(m => m.CssModule)
   },
   {
-    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de css
+    // hacer el routing con lazy loading, importo el modulo que contiene todo lo referente al componente de js
     path: 'js',
     loadChildren: () => import('./containers/js/js.module').then(m => m.JsModule)
   },
-  { path: '**', redirectTo: '/home' }
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
